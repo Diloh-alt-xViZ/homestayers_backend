3a7dbf6f3457413373061a16f55f8b7f4eb5d1d9
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2024-12-04 16:26:09 +0200

    Update Lodge.java

--- src/main/java/com/developer/homestayersbackend/entity/Lodge.java
+++ src/main/java/com/developer/homestayersbackend/entity/Lodge.java
@@ -1,10 +1,18 @@
 package com.developer.homestayersbackend.entity;
 
-
+import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+
+@EqualsAndHashCode(callSuper=true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Lodge extends Property{
 
 }
