900b8e66ed2f3df586fd26634be35fe3b108bbd2
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2024-12-23 04:16:45 +0200

    Update TwilioService.java

--- src/main/java/com/developer/homestayersbackend/service/api/TwilioService.java
+++ src/main/java/com/developer/homestayersbackend/service/api/TwilioService.java
@@ -2,12 +2,12 @@ package com.developer.homestayersbackend.service.api;
 
 import com.developer.homestayersbackend.dto.OtpResponse;
 import com.developer.homestayersbackend.dto.PhoneNumber;
+import com.twilio.exception.TwilioException;
 
 public interface TwilioService {
-    OtpResponse sendVerificationCode(String phoneNumber);
-    void sendBookingNotification(PhoneNumber phoneNumber, String message, PhoneNumber phoneNumber1);
-    void sendBookingDenialNotification(PhoneNumber guestPhoneNumber, String message);
+    OtpResponse sendVerificationCode(String phoneNumber, String otp)throws TwilioException;
+    void sendBookingNotification(PhoneNumber phoneNumber, String message, PhoneNumber phoneNumber1) throws Exception;
+    void sendBookingDenialNotification(PhoneNumber guestPhoneNumber, String message) throws Exception;
     boolean verifyCode(String phoneNumber);
-
-    void sendBookingApprovalNotification(PhoneNumber phoneNumber, String approvalMessage);
+    void sendBookingApprovalNotification(PhoneNumber phoneNumber, String approvalMessage) throws Exception;
 }
