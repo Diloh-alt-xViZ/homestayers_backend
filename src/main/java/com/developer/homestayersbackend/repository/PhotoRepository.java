220ccc7c7392de4776fa81173d2cad3cc08df8f3
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2024-12-27 17:50:46 +0200

    Update PhotoRepository.java

--- src/main/java/com/developer/homestayersbackend/repository/PhotoRepository.java
+++ src/main/java/com/developer/homestayersbackend/repository/PhotoRepository.java
@@ -3,7 +3,12 @@ package com.developer.homestayersbackend.repository;
 import com.developer.homestayersbackend.entity.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
     Photo findPhotoByUrl(String url);
+    List<Photo> findAllByUrlContaining(String url);
 }
