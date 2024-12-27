7f6817d2bd75f432e3fcbcd706c4200b43eb98b0
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2024-12-27 16:31:53 +0200

    Update RoomRepository.java

--- src/main/java/com/developer/homestayersbackend/repository/RoomRepository.java
+++ src/main/java/com/developer/homestayersbackend/repository/RoomRepository.java
@@ -2,6 +2,12 @@ package com.developer.homestayersbackend.repository;
 
 import com.developer.homestayersbackend.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    List<Room> findAllByIdIn(List<Long> ids);
 }
