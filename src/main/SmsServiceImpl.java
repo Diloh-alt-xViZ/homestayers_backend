a87ac4b225ee616706465e36739289b79d8eed75
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2024-12-28 16:09:40 +0200

    Update SmsServiceImpl.java

--- src/main/SmsServiceImpl.java
+++ src/main/SmsServiceImpl.java
@@ -2,6 +2,7 @@ package com.developer.homestayersbackend.service.impl;
 
 import com.africastalking.sms.Recipient;
 import com.developer.homestayersbackend.config.SmsConfig;
+import com.developer.homestayersbackend.dto.OtpResponse;
 import com.developer.homestayersbackend.service.api.SmsService;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +21,19 @@ public class SmsServiceImpl implements SmsService {
             e.printStackTrace();
         }
         return null;
+    }
+
+
+    @Override
+    public OtpResponse sendVerificationCode(String phoneNumber, String code) {
+        OtpResponse otpResponse = new OtpResponse(code,phoneNumber);
+        String message = "Dear Customer, your verification code is "+ code+", valid for 5 minutes.";
+        try{
+            SmsConfig.getSmsService().send(message,new String[]{phoneNumber},true);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return otpResponse;
     }
 }
