adc4c23f5dff263516f26f2cd0e81d4b2f24222b
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2024-12-30 17:05:44 +0200

    Update PhotoService.java

--- src/main/java/com/developer/homestayersbackend/service/api/PhotoService.java
+++ src/main/java/com/developer/homestayersbackend/service/api/PhotoService.java
@@ -3,9 +3,13 @@ package com.developer.homestayersbackend.service.api;
 import com.developer.homestayersbackend.dto.PhotoDeleteDto;
 import com.developer.homestayersbackend.entity.Photo;
 
+import java.util.List;
+
 public interface PhotoService {
     Photo createPhoto(Photo photo);
 
     String deleteRoomPhotos(PhotoDeleteDto dto);
+
+    String deleteMultiplePhotos(List<PhotoDeleteDto> dto);
 }
 
