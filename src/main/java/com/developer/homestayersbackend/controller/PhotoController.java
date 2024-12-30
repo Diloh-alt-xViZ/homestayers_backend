897c3be7c4db5430990c7153d0fecac5ff35ef19
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2024-12-30 17:05:20 +0200

    Update PhotoController.java

--- src/main/java/com/developer/homestayersbackend/controller/PhotoController.java
+++ src/main/java/com/developer/homestayersbackend/controller/PhotoController.java
@@ -6,7 +6,12 @@ import com.developer.homestayersbackend.service.api.PhotoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -23,5 +28,12 @@ public class PhotoController {
         return ResponseEntity.ok(photoService.deleteRoomPhotos(dto));
     }
 
+    @PreAuthorize("hasAuthority('USER')")
+    @DeleteMapping("/rooms/deleteMultiplePhotos")
+    public ResponseEntity<String> deleteMultiplePhotos(@RequestBody List<PhotoDeleteDto> dto) {
+
+
+        return ResponseEntity.ok(photoService.deleteMultiplePhotos(dto));
+    }
 
 }
