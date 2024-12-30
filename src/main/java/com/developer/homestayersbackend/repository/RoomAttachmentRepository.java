fe4944a005df3978beee5559b56b9069a63498a4
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2024-12-30 13:25:15 +0200

    Update RoomAttachmentRepository.java

--- src/main/java/com/developer/homestayersbackend/repository/RoomAttachmentRepository.java
+++ src/main/java/com/developer/homestayersbackend/repository/RoomAttachmentRepository.java
@@ -1,9 +1,15 @@
 package com.developer.homestayersbackend.repository;
 
+import com.developer.homestayersbackend.entity.AttachmentType;
 import com.developer.homestayersbackend.entity.RoomAttachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RoomAttachmentRepository extends JpaRepository<RoomAttachment, Long> {
+
+    Optional<RoomAttachment> findRoomAttachmentByAttachmentType(AttachmentType attachmentType);
 }
