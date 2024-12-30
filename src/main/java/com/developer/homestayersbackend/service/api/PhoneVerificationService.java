c6cc028e2b62a7af08eb152279e6aebdecdb19cf
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2024-12-30 13:31:42 +0200

    Update PhoneVerificationService.java

--- src/main/java/com/developer/homestayersbackend/service/api/PhoneVerificationService.java
+++ src/main/java/com/developer/homestayersbackend/service/api/PhoneVerificationService.java
@@ -1,9 +1,12 @@
 package com.developer.homestayersbackend.service.api;
 
 import com.developer.homestayersbackend.controller.auth.AuthenticationResponse;
+import com.developer.homestayersbackend.dto.PhoneNumberAuth;
 import com.developer.homestayersbackend.entity.PhoneVerification;
 
 public interface PhoneVerificationService {
     public AuthenticationResponse verifyPhone(String phone, String verificationCode);
     public PhoneVerification getPhoneVerification(String phone);
+
+    String verifyNewPhone(PhoneNumberAuth authRequest, Long id);
 }
