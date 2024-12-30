ac30d91af8382cddff42eac18f6f2e8de20f2893
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2024-12-30 17:04:26 +0200

    Update PhotoServiceImpl.java

--- src/main/java/com/developer/homestayersbackend/service/impl/PhotoServiceImpl.java
+++ src/main/java/com/developer/homestayersbackend/service/impl/PhotoServiceImpl.java
@@ -11,7 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -28,17 +30,38 @@ public class PhotoServiceImpl implements PhotoService {
 
     @Transactional
     @Override
+    public String deleteMultiplePhotos(List<PhotoDeleteDto> dtos) {
+        Room room = roomRepository.findById(dtos.get(0).getId()).orElseThrow(RoomNotFoundException::new);
+
+        dtos.stream()
+                .map((dto -> {
+                  return  photoRepository.findAllByUrlContaining(dto.getUri());
+                }))
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .filter(room.getPhotos()::contains)
+                .forEach(room.getPhotos()::remove);
+        roomRepository.save(room);
+        return "Photos Deleted Successfully";
+    }
+
+
+
+    @Transactional
+    @Override
     public String deleteRoomPhotos(PhotoDeleteDto dto) {
 
         Room room = roomRepository.findById(dto.getId()).orElseThrow(RoomNotFoundException::new);
         List<Photo> photos = photoRepository.findAllByUrlContaining(dto.getUri());
-
+        System.out.println("Photos:"+photos);
         if(!photos.isEmpty()) {
             List<Photo> roomPhotos = photos.stream().filter(photo -> room.getPhotos().contains(photo)).collect(Collectors.toList());
+            System.out.println("Room Photos:"+roomPhotos);
 
-            room.getPhotos().removeAll(roomPhotos);
-            photoRepository.deleteAll(roomPhotos);
-            roomRepository.save(room);
+            if(!roomPhotos.isEmpty()) {
+                room.getPhotos().removeAll(roomPhotos);
+                roomRepository.save(room);
+            }
 
         }
         return "Delete photo successfully";
