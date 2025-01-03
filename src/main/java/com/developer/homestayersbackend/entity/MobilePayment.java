a219e3e06efa74532d2cf01b8bb070c863175145
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2025-01-03 14:15:02 +0200

    Added a new api to handle payments

---
+++ src/main/java/com/developer/homestayersbackend/entity/MobilePayment.java
@@ -0,0 +1,24 @@
+package com.developer.homestayersbackend.entity;
+
+
+import com.developer.homestayersbackend.dto.PhoneNumber;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import zw.co.paynow.constants.MobileMoneyMethod;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper=true)
+public class MobilePayment extends Payments {
+
+    private PhoneNumber phoneNumber;
+    @Enumerated(EnumType.STRING)
+    private MobileProvider provider;
+}
