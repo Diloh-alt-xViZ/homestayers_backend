55407c76489a88fa047c47d7037248a5570a1f49
Dillan Bundo <78935543+Diloh-alt-xViZ@users.noreply.github.com>
2025-01-08 18:00:49 +0200

    Update UserService.java

--- src/main/java/com/developer/homestayersbackend/service/api/UserService.java
+++ src/main/java/com/developer/homestayersbackend/service/api/UserService.java
@@ -51,4 +51,6 @@ public interface UserService {
     boolean checkUserDetails(Long id);
 
     AuthenticationResponse loginPhoneUser(PhoneLoginRequest req);
+
+    boolean checkPhoneProfile(String phoneNumber);
 }
